fbd422fe56545e3a8c2b85834d093016dd564579
Richy <richy@webcomm.com.tw>
2023-08-04 20:09:15 +0800

    new file ContentViewController.swift

---
+++ iOS_MyBook/ContentViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ContentViewController.swift
+//  iOS_MyBook
+//
+//  Created by 王麒翔 on 2023/8/4.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+    
+    @IBOutlet weak var mainImageView: UIImageView!
+    
+    var nowPageNumber = 0 // 目前頁數
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        mainImageView.image = UIImage(named: "\(nowPageNumber)")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
