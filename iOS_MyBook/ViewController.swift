6546068129f76fe6e2447d17937b720354a875b3
Richy <richy@webcomm.com.tw>
2023-08-04 21:40:21 +0800

    註解

--- iOS_MyBook/ViewController.swift
+++ iOS_MyBook/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController, UIPageViewControllerDataSource {
     
+    // 前一頁
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         let vc = viewController as? ContentViewController
         guard var index = vc?.nowPageNumber else { return nil }
@@ -20,6 +21,7 @@ class ViewController: UIViewController, UIPageViewControllerDataSource {
         }
     }
     
+    // 下一頁
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
         let vc = viewController as? ContentViewController
         guard var index = vc?.nowPageNumber else { return nil }
@@ -43,21 +45,26 @@ class ViewController: UIViewController, UIPageViewControllerDataSource {
         // Do any additional setup after loading the view.
         
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        // UIPageViewController
         let pageViewController = storyboard.instantiateViewController(withIdentifier: "PageViewController") as? UIPageViewController
         pageViewController?.dataSource = self // dataSource
         pageViewController?.view.frame = self.view.frame // 設定 pageViewController畫面跟 ViewController一樣大
         if let okPageViewController = pageViewController {
             addChild(pageViewController!) // 加入到 view controller當中
-            view.addSubview(pageViewController!.view)
+            view.addSubview(pageViewController!.view) // show到 ViewController畫面上
         }
         pageViewController?.didMove(toParent: self) //
         
         guard let startPage = viewControllerAtIndex(index: 0) else { return }
+        // 設定初始頁面
         pageViewController?.setViewControllers([startPage], direction: .forward, animated: true, completion: nil)
     }
     
     func viewControllerAtIndex (index:Int) -> ContentViewController? {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        // ContentViewController
         let contentViewController = storyboard.instantiateViewController(withIdentifier: "MainContentViewController") as? ContentViewController
         contentViewController?.nowPageNumber = index
         return contentViewController
