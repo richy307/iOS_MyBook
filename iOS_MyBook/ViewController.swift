16c648e685acf40d80573b07b36ce3841724e295
Richy <richy@webcomm.com.tw>
2023-08-04 21:07:36 +0800

    pageViewController?.setViewControllers

--- iOS_MyBook/ViewController.swift
+++ iOS_MyBook/ViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var pageViewController: UIViewController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let pageViewController = storyboard.instantiateViewController(withIdentifier: "PageViewController") as? UIPageViewController
+        pageViewController?.view.frame = self.view.frame // 設定 pageViewController畫面跟 ViewController一樣大
+        if let okPageViewController = pageViewController {
+            addChild(pageViewController!) // 加入到 view controller當中
+            view.addSubview(pageViewController!.view)
+        }
+        pageViewController?.didMove(toParent: self) //
+        
+        guard let startPage = viewControllerAtIndex(index: 0) else { return }
+        pageViewController?.setViewControllers([startPage], direction: .forward, animated: true, completion: nil)
+    }
     
     func viewControllerAtIndex (index:Int) -> ContentViewController? {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
